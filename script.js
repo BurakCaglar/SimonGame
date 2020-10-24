c273ea09e3d738c8ba2cca2adbb28f1802537999
BurakCaglar <burakcaglar@yandex.com>
2020-10-24 23:46:26 +0300

    baslangic

---
+++ script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+let userClickedPattern = [];
+
+let started = false;
+
+let level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level" + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  let userChosenColour = $(this).attr("id");
+  
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
