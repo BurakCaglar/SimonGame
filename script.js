a6c0bece6a52ec9130e7d32d31bc92cf7f3958e7
BurakCaglar <61995612+BurakCaglar@users.noreply.github.com>
2020-10-26 20:06:27 +0300

    level alanı, arkaplan, oyun dizisi

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 "use strict";
 
 let buttonColours = ["red", "blue", "green", "yellow"];
-
 let gamePattern = [];
 let userClickedPattern = [];
-
 let started = false;
-
 let level = 0;
 
 $(document).keypress(function() {
@@ -25,30 +22,60 @@ $(".btn").click(function() {
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text(`Oyun bitti. Tekrar oynamak için bir tuşa basınız. Leveliniz: ${level}`);
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
   level++;
+
   $("#level-title").text("Level " + level);
-
-  let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColour = buttonColours[randomNumber];
-
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
   playSound(randomChosenColour);
-}
-
-function playSound(name) {
-  let audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
 }
 
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
+
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
